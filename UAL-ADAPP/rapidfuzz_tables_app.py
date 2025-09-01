e3dc72c5d647e41554689df37bab66a766e3facd
Yahir2003L <lopezyahir884@gmail.com>
2025-09-01 10:40:10 -0600

    Entregable 7 - Output options (DataFrame or List)

--- UAL-ADAPP/rapidfuzz_tables_app.py
+++ UAL-ADAPP/rapidfuzz_tables_app.py
@@ -1,6 +1,17 @@
 from db_fuzzy_match import connect_to_db, fuzzy_match, execute_dynamic_matching
 import pandas as pd
 
+def display_results(results, output_format="dataframe"):
+    if output_format == "dataframe":
+        df = pd.DataFrame(results)
+        print(df)
+    elif output_format == "dictionary":
+        for record in results:
+            print(record)
+    else:
+        print("formato invalido. Usa 'dataframe' o 'dictionary'.")
+    
+
 params_dict = {
     "db_type": "mysql",
     "server": "localhost",
@@ -17,5 +28,5 @@ params_dict = {
 }
 
 resultados = execute_dynamic_matching(params_dict, score_cutoff=70)
-df = pd.DataFrame(resultados)
-print(df)
+output_format = input("Elige formato de salida (dataframe/dictionary): ").strip().lower()
+display_results(resultados, output_format= output_format)
